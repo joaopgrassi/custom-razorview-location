1494f0b62371f0449a85d2fed91afb1cbe452448
Joao Grassi <joaop.grassi@gmail.com>
2018-05-10 18:00:49 +0200

    Added new Customer feature with it's own set of views and controller.

---
+++ src/MyApp.Web/Features/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.ViewModels;
+
+namespace MyApp.Web.Features.Customer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View("Index");
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CustomerViewModel customer)
+        {
+            if (ModelState.IsValid)
+            {
+                // do something here...
+            }
+            return View("Index");
+        }
+    }
+}
